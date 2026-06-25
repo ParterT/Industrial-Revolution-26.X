f6955fceecf522378a85bb26cfec025362b67596
ParterT <gad59922@gmail.com>
2026-06-25 12:43:38 +0300

    new tool material

---
+++ src/main/java/net/partert/ind_nova/item/tool/MToolTiers.java
@@ -0,0 +1,10 @@
+package net.partert.ind_nova.item.tool;
+
+import net.minecraft.world.item.ToolMaterial;
+import net.partert.ind_nova.tags.MTags;
+
+public class MToolTiers {
+    public static final ToolMaterial STEEL = new ToolMaterial(MTags.Blocks.INCORRECT_FOR_STEEL_TOOL,
+            890, 7f, 3f, 18, MTags.Items.STEEL_REPAIRABLE);
+
+}
